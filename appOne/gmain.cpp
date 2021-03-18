3da7d36ec0ce91ce17af74711c6ad71ecfe6ff5d
Coding-Ocean <eijo524@gmail.com>
2021-03-18 17:52:04 +0900

    add cylinder vertices

--- appOne/gmain.cpp
+++ appOne/gmain.cpp
@@ -3,6 +3,32 @@
 struct POS {
     float x, y, z;
 };
+void setSphere(struct POS* op, int numCorners, int numRings) {
+    float deg = 360.0f / numCorners;
+    angleMode(DEGREES);
+    for (int j = 0; j < numRings; j++) {
+        float r = sin(deg * j);
+        float z = cos(deg * j);
+        for (int i = 0; i < numCorners; i++) {
+            int k = j * numCorners + i;
+            op[k].x = sin(deg * i) * r;
+            op[k].y = cos(deg * i) * r;
+            op[k].z = z;
+        }
+    }
+}
+void setCylinder(struct POS* op, int numCorners, int numRings) {
+    float deg = 360.0f / numCorners;
+    angleMode(DEGREES);
+    for (int j = 0; j < numRings; j++) {
+        for (int i = 0; i < numCorners; i++) {
+            int k = j * numCorners + i;
+            op[k].x = sin(deg * i);
+            op[k].y = cos(deg * i);
+            op[k].z = 1.0f - 2.0f / (numRings - 1) * j;
+        }
+    }
+}
 //ゲームメイン
 void gmain() {
     window(1000, 1000, full);
@@ -14,24 +40,22 @@ void gmain() {
     struct POS* op = new POS[numVertices];
     float deg = 360.0f / numCorners;
     angleMode(DEGREES);
-    for (int j = 0; j < numRings; j++) {
-        float r = sin(deg * j);
-        float z = cos(deg * j);
-        for (int i = 0; i < numCorners; i++) {
-            int k = j * numCorners + i;
-            op[k].x = sin(deg * i) * r;
-            op[k].y = cos(deg * i) * r;
-            op[k].z = z;// 1.0f - 2.0f / (numRings - 1) * j;
-        }
-    }
+    setSphere(op, numCorners, numRings);
     //変更後の頂点位置の入れ物（ループするたびに変化する）
     struct POS* p = new POS[numVertices];
     //元の頂点位置を回転させる角度
     deg = 0;
     //このstateが0の時Y軸、1の時X軸回転する
     int state = 0;
+    int sw = 1;
     //メインループ
     while (notQuit) {
+        //頂点位置変更
+        if (isTrigger(KEY_SPACE)) {
+            sw = 1 - sw;
+            if (sw == 0)setCylinder(op, numCorners, numRings);
+            else setSphere(op, numCorners, numRings);
+        }
         //あらかじめsin,cosを計算しておく
         float s = sin(deg);
         float c = cos(deg);
@@ -67,8 +91,8 @@ void gmain() {
         stroke(160, 200, 255);
         for (int i = 0; i < numVertices; i++) {
             //頂点
-            strokeWeight(6);
-            mathPoint(p[i].x, p[i].y);
+            //strokeWeight(6);
+            //mathPoint(p[i].x, p[i].y);
             //リングの頂点を線で結ぶ
             int j = i + 1;
             if (j % numCorners == 0)j -= numCorners;
